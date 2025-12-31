a3431c42686d1c21c6b462e69f7a784061453c78
shehlizah <shehlizah1@gmail.com>
2025-12-31 15:46:10 +0500

    token error resolved

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+// use Laravel\Sanctum\HasApiTokens;
 use App\Http\Middleware\CheckActivePackage;
 
-class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
