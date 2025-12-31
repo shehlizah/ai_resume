f66eaaedaae56c504873ad7b563421e50008c9c1
shehlizah <shehlizah1@gmail.com>
2025-12-31 15:56:27 +0500

    admin token

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,4 +1,4 @@
-use Laravel\Sanctum\HasApiTokens;
+
 <?php
 
 namespace App\Models;
@@ -7,10 +7,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-// use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 use App\Http\Middleware\CheckActivePackage;
 
 
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
